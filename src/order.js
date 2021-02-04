a96ee034ba579c00659c9cde83af0a3065128a63
Angela <angela.battillo@gmail.com>
2021-02-04 15:57:12 -0700

    Passing beginning of test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,18 @@
+function takeOrder(order, delivery){
+  if (delivery.length <= 2){
+  delivery.push(order)
+} else {
+  return order;
+ }
+}
+
+function refundOrder(){
+  
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
