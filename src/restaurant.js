ac8f843b0eb0cf97de97fa597b673799a8fb1e7f
Angela <angela.battillo@gmail.com>
2021-02-05 20:22:10 -0700

    refactor prior functionality with psuedocoding for next suite

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,29 @@
+function createRestaurant(entitle){
+  return {
+    name: entitle,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+}
 
+function addMenuItem(callIt, item){
+  if (callIt.menus[item.type].includes(item)){
+    return callIt;
+  } else {
+    return callIt.menus[item.type].push(item);
+  }
+}
+
+function removeMenuItem(title, dish){
+  title.menus[dish.type].pop();
+
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
