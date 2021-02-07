c2ae7cd0d40359d3eb16776d15accd254795ee2d
Angela <angela.battillo@gmail.com>
2021-02-07 10:20:17 -0700

    Passing all save the last two tests with refactoring

--- src/Chef.js
+++ src/Chef.js
@@ -10,6 +10,14 @@ class Chef {
     return  `Hello, ${name}!`
   }
  }
+ checkForFood(foodItem){
+    if (foodItem === foodItem) {
+      console.log(foodItem);
+     return  `Yes, we're serving ${foodItem.name} today!`;
+   }
+     return "Sorry, we aren't serving Quiche today."
+   }
+
 }
 
 module.exports = Chef;
